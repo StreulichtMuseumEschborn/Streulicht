0352d0206c089da16e1de1a01b6443fdfdfd01e0
StreulichtMuseumEschborn <128782614+StreulichtMuseumEschborn@users.noreply.github.com>
2023-03-28 21:12:05 +0200

    fertig yippee

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Quelle Slideshow: https://www.w3schools.com/w3css/w3css_slideshow.asp */
 let slideIndex = 1;
 
 showSlides(slideIndex);
@@ -15,17 +16,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   console.log(slides[slideIndex-1]);
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
 
